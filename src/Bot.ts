fda09ff5a6358efd4c21a204c20ee8561cdb76f2
Gnlow <gnlow@protonmail.com>
2022-07-14 21:28:52 +0900

    it works

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,8 @@
-import type { ApiLoginParams } from "https://cdn.skypack.dev/types-mediawiki/api_params?dts"
+import { Lw } from "https://denopkg.com/gnlow/lazywrap@main/mod.ts"
 
 import urlcat, { ParamMap } from "https://deno.land/x/urlcat/src/index.ts"
 
 import {
-  Cookie,
   CookieJar,
   wrapFetch,
 } from "https://deno.land/x/another_cookiejar@v4.1.4/mod.ts"
@@ -12,39 +11,64 @@ interface BotOptions {
     apiUrl: string
     username: string
     password: string
+    userAgent: string
 }
 
 export class Bot {
     apiUrl
     username
     password
+    userAgent
 
     cookieJar = new CookieJar()
     fetch = wrapFetch({cookieJar: this.cookieJar})
+
+    @Lw("getLoginToken")
+    declare loginToken: Lw<string>
+
+    @Lw("getCsrfToken")
+    declare csrfToken: Lw<string>
+
 
     constructor(options: BotOptions) {
         this.apiUrl = options.apiUrl
         this.username = options.username
         this.password = options.password
+        this.userAgent = options.userAgent
     }
     
     async GET(params: ParamMap) {
         return await this.fetch(
-            urlcat(this.apiUrl, params)
+            urlcat(this.apiUrl, params),
+            {
+                headers: {
+                    "user-agent": `${this.userAgent} deno-mw/0.1.0`
+                }
+            }
         )
     }
     async POST(params: ParamMap) {
+        const {
+            action,
+            format,
+            ...rest
+        } = params
         return await this.fetch(
-            this.apiUrl,
+            urlcat(this.apiUrl, {action, format}),
             {
                 method: "POST",
-                body: JSON.stringify(params),
+                body: urlcat("", rest),
+                headers: {
+                    "user-agent": this.userAgent,
+                    "content-type": "application/x-www-form-urlencoded"
+
+                }
             }
         )
     }
 
-    async login() {
-        const loginToken = await this.GET({
+    async getLoginToken() {
+        return this.loginToken = await this.GET({
             action: "query",
             meta: "tokens",
             type: "login",
@@ -52,14 +76,35 @@ export class Bot {
         })
         .then(res => res.json())
         .then(res => res.query.tokens.logintoken)
-
+    }
+    async login() {
         return await this.POST({
             action: "login",
             lgname: this.username,
             lgpassword: this.password,
-            lgtoken: loginToken,
+            lgtoken: await this.loginToken,
             format: "json",
         })
-        .then(res => res.body)
+        .then(res => res.json())
+    }
+    async getCsrfToken() {
+        return this.csrfToken = await this.GET({
+            action: "query",
+            meta: "tokens",
+            format: "json",
+        })
+        .then(res => res.json())
+        .then(res => res.query.tokens.csrftoken)
+    }
+    async editRequest() {
+        return await this.POST({
+            action: "edit",
+            title: "연습장:Gnlow/API",
+            appendtext: "테스트~~~~!",
+            token: await this.csrfToken,
+            format: "json",
+        })
+        .then(res => res.text())
+        .catch(e => {throw e})
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,13 @@
 import { Bot } from "../mod.ts"
-import secrets, { username, password } from "../secrets.json" assert { type: "json" }
+import secrets from "../secrets.json" assert { type: "json" }
 
 const testBot = new Bot({
     apiUrl: "http://wiki.shtelo.org/api.php",
     username: secrets.username,
     password: secrets.password,
+    userAgent: "TadiaBot/0.1.0 (https://github.com/Yirule/Tadia)"
 })
 
 console.log(await testBot.login())
+console.log(await testBot.csrfToken)
+//console.log(await testBot.editRequest())
